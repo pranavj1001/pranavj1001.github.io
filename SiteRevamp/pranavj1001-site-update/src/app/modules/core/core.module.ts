9ede273affddf6a2aa84789a1058e16dab25c75b
Pranav Jain <pranavj1001@gmail.com>
2018-11-18 15:04:52 +0530

    add home component

--- SiteRevamp/pranavj1001-site-update/src/app/modules/core/core.module.ts
+++ SiteRevamp/pranavj1001-site-update/src/app/modules/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     CoreRoutingModule
